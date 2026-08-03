f39349d031dffbee2e478bae24e31053bde3a86d
jsfmytg <jsfmytg@126.com>
2026-08-04 00:07:19 +0800

    -

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,6 +124,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
